085ffefaf4bfcf27cd35c62a51225a94e8f37ca7
EmadEJ <s.emad.emamjomeh@gmail.com>
2024-04-02 17:23:36 +0330

    implemented spell check

--- Logic/core/spell_correction.py
+++ Logic/core/spell_correction.py
@@ -1,3 +1,6 @@
+import json
+
+
 class SpellCorrection:
     def __init__(self, all_documents):
         """
@@ -28,7 +31,12 @@ class SpellCorrection:
         """
         shingles = set()
         
-        # TODO: Create shingle here
+        if len(word) < k:
+            shingles.add(word)
+
+        for i in range(len(word) - k + 1):
+            shingle = word[i:i+k]
+            shingles.add(shingle)
 
         return shingles
     
@@ -48,10 +56,7 @@ class SpellCorrection:
         float
             Jaccard score.
         """
-
-        # TODO: Calculate jaccard score here.
-
-        return
+        return len(first_set.intersection(second_set)) / len(first_set.union(second_set))
 
     def shingling_and_counting(self, all_documents):
         """
@@ -72,7 +77,16 @@ class SpellCorrection:
         all_shingled_words = dict()
         word_counter = dict()
 
-        # TODO: Create shingled words dictionary and word counter dictionary here.
+        for doc in all_documents:
+            doc = doc.lower()
+            words = doc.split()
+            for word in words:
+                if word_counter.get(word) is None:
+                    word_counter[word] = 0
+                word_counter[word] += 1
+        
+        for word in word_counter.keys():
+            all_shingled_words[word] = self.shingle_word(word)
                 
         return all_shingled_words, word_counter
     
@@ -91,9 +105,18 @@ class SpellCorrection:
             5 nearest words.
         """
         top5_candidates = list()
+        word_shingle = self.shingle_word(word.lower())
 
-        # TODO: Find 5 nearest candidates here.
-
+        for candidate, candidate_shingled in self.all_shingled_words.items():
+            if len(top5_candidates) < 5:
+                top5_candidates.append(candidate)
+            else:
+                for top in top5_candidates:
+                    if self.jaccard_score(word_shingle, candidate_shingled) > self.jaccard_score(word_shingle, self.shingle_word(top)):
+                        top5_candidates.remove(top)
+                        top5_candidates.append(candidate)
+                        break
+        
         return top5_candidates
     
     def spell_check(self, query):
@@ -111,7 +134,33 @@ class SpellCorrection:
             Correct form of the query.
         """
         final_result = ""
-        
-        # TODO: Do spell correction here.
 
-        return final_result
+        for word in query.split():
+            candidates = self.find_nearest_words(word)
+            # print(candidates)
+            word = word.lower()
+            top, top_score = '', 0
+            for candidate in candidates:
+                if candidate == word:
+                    top = candidate
+                    top_score = 1
+                    break
+                jaccard_score = self.jaccard_score(self.shingle_word(word), self.shingle_word(candidate))
+                score = jaccard_score * self.word_counter[candidate] / max([self.word_counter[c] for c in candidates])
+                # print(jaccard_score, score)
+                if score > top_score:
+                    top = candidate
+                    top_score = score
+            final_result = final_result + top + " "
+
+        return final_result.strip()
+    
+
+with open("preprocessed_docs.json", "r") as FILE:
+    all_documents = json.load(FILE)
+
+doc_strings = [' '.join(doc['stars'] + doc['genres'] + doc['summaries']) for doc in all_documents if len(doc['summaries']) > 0]
+
+sc = SpellCorrection(doc_strings)
+
+print(sc.spell_check("The amazing soectacular unbeleivable astonishing alright breakaing"))
